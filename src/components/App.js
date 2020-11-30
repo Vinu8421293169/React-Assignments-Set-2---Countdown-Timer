1d8611a9404aa7ea04dce844c565a04a5f52e5a3
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-01 00:14:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,36 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import LocationDisplayComponent from "./LocationDisplayComponent";
+function About() {
+  return <div>You are on the about page.</div>;
+}
+function Home() {
+  return <div>You are home.</div>;
+}
 
-function App() {
-  const [count, setCount] = useState();
-  let value;
-  var interval;
+function Invalid() {
+  return <div>No match</div>;
+}
 
-  function handleKeyDown(event) {
-    if (event.key === "Enter") {
-      clearInterval(interval);
-      let val = parseInt(event.target.value);
-      value = val === val ? val : 0;
-      interval = setInterval(() => {
-        if (value <= 0) clearInterval(interval);
-        setCount(value--);
-      }, 1000);
-    }
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div id="main">{/* Do not remove the main div */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+            <Route path="/" component={Invalid} />
+          </Switch>
+          <LocationDisplayComponent />
+        </div>
+      </>
+    );
   }
-
-  return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for
-          <input id="timeCount" onKeyDown={(event) => handleKeyDown(event)} />
-          sec.
-        </h1>
-      </div>
-      <div id="current-time">{count}</div>
-    </div>
-  );
 }
 
 export default App;
+export const LocationDisplay = LocationDisplayComponent;
