8da503cd163c82e7f199732b66e2fc7e523de724
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-01 17:56:59 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState, useEffect } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      console.log("enter");
+    }
+  };
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div className="wrapper">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyDown={(event) => handleKeyDown(event)}
+          />{" "}
+          sec.
+        </h1>
+      </div>
+      <div id="current-time">{/* remaining time */}</div>
+    </div>
+  );
+};
 
 export default App;
